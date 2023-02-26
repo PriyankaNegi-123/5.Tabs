a2f33fbe1a0c04c8d3eb2403e653dcefdce027e6
Priyanka Negi <negipriyanka2211@gmail.com>
2023-02-26 22:51:50 +0530

    tabs

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import './index.css';
+import {FaAngleDoubleRight} from 'react-icons/fa'
+import { v4 as uuidv4 } from 'uuid';
+
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [people, setPeople] = useState([]);
+    const [value, setValue] = useState(0)
+
+    const fetchPeople = async()=>{
+        const response = await fetch(url);
+        const data = await response.json();
+        setPeople(data);
+        setIsLoading(false);
+    };
+
+    useEffect(()=>{
+        fetchPeople();
+    }, [])
+   
+    if(isLoading){
+  return (
+    <section className = 'section loading'>
+    <h1>Loading...</h1>
+    </section>
+  )}
+
+  const{company, dates, duties, title} = people[value];
+
+  return(
+    <section className='section'>
+        <div className='title'>
+            <h2>Experience</h2>
+            <div className='underline'></div>
+        </div>
+        <div className='jobs-center'>
+            <div className='btn-container'>
+                {people.map((person,index)=>{
+                    return <button className='job-btn' onClick={()=>setValue(index)} key={uuidv4()}>{person.company}</button>
+                })}
+            </div>
+
+            <article className='job-info'>
+                <h3>{title}</h3>
+                <h4>{company}</h4>
+                <p className='job-date'>{dates}</p>
+                {duties.map((duty)=>{
+                    return(
+                        <div className='job-desc' key={uuidv4()}>
+                            <FaAngleDoubleRight className='job-icon'/>
+                            <p>{duty}</p>
+                        </div>
+                    )
+                })}
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default App
